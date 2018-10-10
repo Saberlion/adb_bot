084d21dc7358393ab968621cf6d4c2c329fa5888
saberlion <fxre@qq.com>
2018-10-10 23:32:51 +0800

    post screenshot to qiniu and send msg to wechat

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-from common.auto_adb import auto_adb
-from common.screenshot import  pull_screenshot
+import uuid
 
+from common.auto_adb import auto_adb
+from common.config import open_accordant_config
+from common.qiniu_upload import upload_to_qiniu
+from common.screenshot import pull_screenshot
+from common.serverchan import send_server_chan_msg
 
 adb = auto_adb()
+conf = open_accordant_config('config/default.json')
 
 
 def unlock_screen():
     adb.run('shell input keyevent 26')  # press power
-    adb.swipe(100, 500, 600, 500)  # swipe to unlock
+    adb.swipe(100, 500, 600, 500, 200)  # swipe to unlock
 
 
 def start_activity():
@@ -18,5 +23,14 @@ def start_activity():
     adb.run(cmd)
 
 
+def upload(localfile, key):
+    upload_to_qiniu(conf['qiniu']['access_key'], conf['qiniu']['secret_key'], 'saberlion', localfile, key)
+
+
+unlock_screen()
 start_activity()
 pull_screenshot()
+key = 'screenshot{rand}.png'.format(rand=uuid.uuid1())
+upload('screenshot.png', key)
+content = '![pic]({url})'.format(url='http://saberlion.u.qiniudn.com/'+key)
+send_server_chan_msg(conf['serverchan']['sckey'], "Test", content)
