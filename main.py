0f45894de063ea8e177e771eb658e72c94d13ea9
Arthur <fxre@gmail.com>
2019-09-18 11:05:10 +0800

    add usb tethering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import uuid
-
+import time
 from common.auto_adb import auto_adb
 from common.config import open_accordant_config
 from common.qiniu_upload import upload_to_qiniu
@@ -11,8 +11,12 @@ adb = auto_adb()
 conf = open_accordant_config('config/default.json')
 
 
-def unlock_screen():
+def lock_screen():
     adb.run('shell input keyevent 26')  # press power
+
+
+def unlock_screen():
+    adb.run('shell input keyevent 82')  # press menu
     adb.swipe(100, 500, 600, 500, 200)  # swipe to unlock
 
 
@@ -28,9 +32,14 @@ def upload(localfile, key):
 
 
 unlock_screen()
+adb.run("shell service call connectivity 33 i32 1 s16 text")
+time.sleep(5)
 start_activity()
+time.sleep(2)
 pull_screenshot()
+
 key = 'screenshot{rand}.png'.format(rand=uuid.uuid1())
 upload('screenshot.png', key)
-content = '![pic]({url})'.format(url='http://saberlion.u.qiniudn.com/'+key)
+lock_screen()
+content = '![pic]({url})'.format(url='http://saberlion.u.qiniudn.com/' + key)
 send_server_chan_msg(conf['serverchan']['sckey'], "Test", content)
